3a7dc72a46a62ca03cc73959818c67dbcaee6eeb
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-01-25 17:05:46 +0800

    initial Commit

---
+++ SentenceMaker/app.js
@@ -0,0 +1,25 @@
+let adjective = "Gigantic";
+let noun = "Hamburger";
+let verb = "Somersaulted";
+let place = "Moon";
+let adjective2 = "Slimy";
+let noun2 = "Octopus";
+
+const firstStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: "+ firstStory);
+
+
+adjective = "Holographic";
+
+noun = "Robot";
+
+verb = "Glitched";
+
+place = "Mars";
+
+adjective2 = "Invisible";
+
+noun2 = "Spaceship";
+
+const secondStory = "Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second story: "+ secondStory);
