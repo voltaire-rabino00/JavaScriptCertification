1080352b32abcc3c56072298ce3775d9dcda2c49
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-02-10 09:19:03 +0800

    To  be Continue

--- EmailMasker/emailMasker.js
+++ EmailMasker/emailMasker.js
@@ -1,3 +1,8 @@
 function maskEmail (email) {
-
+    let emailParts = email.split('@');
+    let username = emailParts[1];
+    let domain = emailParts[0];
+    let maskedUsername = username[1] +  "*".repeat(username.length - 1);
+    return maskedUsername + '@' + domain;
 }
+console.log(maskEmail("myEmail@email.com"))
