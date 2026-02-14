4e29f074526ee2fa54cba8bfa1c8b0ae7a689027
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-02-14 20:00:38 +0800

    done

---
+++ GolfScoreTranslator/golfScore.js
@@ -0,0 +1,33 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore (par, strokes) {
+  if (strokes === 1) {
+    return names[0];
+  }
+  else if (strokes <= par - 2) {
+    return names[1];
+  }
+  else if (strokes === par - 1) {
+    return names[2];
+  }
+  else if (strokes === par) {
+    return names[3];
+  }
+  else if (strokes === par + 1) {
+    return names[4];
+  }
+  else if (strokes === par + 2) {
+    return names[5];
+  }
+  else if (strokes >= par){
+    par+=2;
+    return names[6];
+  }
+}
+
+console.log(golfScore(4, 1)); 
+console.log(golfScore(4, 2)); 
+console.log(golfScore(4, 4)); 
+console.log(golfScore(4, 5)); 
+console.log(golfScore(4, 6)); 
+console.log(golfScore(4, 7)); 
