efad2395522628aedce80e2c94a6219ad4e7d125
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-02-16 21:43:40 +0800

    Lab test

---
+++ QuizGame/quiz.js
@@ -0,0 +1,10 @@
+const questions = [{
+  category: "JavaScript",
+  question: "what is javaScript", 
+  choices: ["A programming language", "A coffee brand", "A movie title", "A car model"],  
+  answer: "A programming language"
+  }];
+
+  function getRandomQuestion (question) {
+    question.length(Math.random(Math.floor()* 4));
+  }
