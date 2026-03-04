69c71a72e66fc6748cb51b3e50b2d9bd30929410
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-03-04 10:00:26 +0800

    Done

--- LargestNumberFinder/largestNum.js
+++ LargestNumberFinder/largestNum.js
@@ -1,6 +1,23 @@
+
 function largestOfAll (arr) {
   let arry = [];
-  for (let i = 0; i < arr.length ; i++){
-    let largest = subArray[0]
+  for (let i = 0; i < arr.length; i++){
+    let largest = arr[i][0];
+    for (let j = 1; j < arr[i].length; j++){
+      if (arr[i][j] > largest){
+        largest = arr[i][j];
+      }
+    }
+    arry.push(largest);
   }
-} 
+  return arry;
+}
+
+let sampleData = [
+  [4, 5, 1, 3], 
+  [13, 27, 18, 26], 
+  [32, 35, 37, 39], 
+  [1000, 1001, 857, 1]
+];
+
+console.log(largestOfAll(sampleData));
