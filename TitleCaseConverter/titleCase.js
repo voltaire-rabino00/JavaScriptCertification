372c4a8a4755ff7c3b85d16198b9028efcbd8c1e
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-03-06 18:20:15 +0800

    arrow function

--- TitleCaseConverter/titleCase.js
+++ TitleCaseConverter/titleCase.js
@@ -1,14 +1,12 @@
-function titleCase(str) {
+const titleCase = (str) => {
   let newStr = str.split(" ");
 
   for (let i = 0; i < newStr.length; i++) {
-    newStr[i].charAt(0).toUpperCase();
-    newStr[i].slice(1).toLowerCase();
+
     newStr[i] = newStr[i].charAt(0).toUpperCase() + newStr[i].slice(1).toLowerCase();
   }
-
   return newStr.join(" ");
-}
+};
 
 let result = "i love javascript";
-console.log(titleCase(result));
+console.log(titleCase(result)); 
