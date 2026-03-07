f1eded9e5b3d68ba4ed23271083eab5dac92a8f5
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-03-07 19:40:54 +0800

    Initial Commit

---
+++ PasswordGeneratorApp/passwordGenerator.js
@@ -0,0 +1,6 @@
+const generatePassword = (length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) => {
+    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const numberChars = '0123456789';
+    const symbolChars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+}
