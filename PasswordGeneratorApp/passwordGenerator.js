fe77389f175297cba3c2cfda73f1595e2925e4bf
voltaire-rabino00 <voltairerabino9@gmail.com>
2026-03-09 20:36:43 +0800

    Initial Commit

--- PasswordGeneratorApp/passwordGenerator.js
+++ PasswordGeneratorApp/passwordGenerator.js
@@ -1,6 +1,14 @@
-const generatePassword = (length, includeUppercase, includeLowercase, includeNumbers, includeSymbols) => {
-    const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
-    const numberChars = '0123456789';
-    const symbolChars = '!@#$%^&*()_+~`|}{[]:;?><,./-=';
-}
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+const generatePassword = (passwordLength) => {
+  let result = ""; 
+  for (let i = 0; i < passwordLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+  return result;
+};
+
+
+const password = generatePassword(7);
+console.log(`Generated password: ${password}`);
